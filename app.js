5a4903cb25fdfe50335d54e99429ef81fedaa523
Reu Li <reujun04@gmail.com>
2026-05-14 23:28:00 -1000

    Add sports JSON route and fix EJS templates

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const fs = require('fs')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
 app.set('layout', 'layout')
 
-app.use(express.static('htmlFiles'))
-
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.get('/events', (req, res) => {
+  res.render('events')
+})
+
+app.get('/sports', (req, res) => {
+  const sports = JSON.parse(fs.readFileSync('./htmlFiles/sports.json', 'utf8'))
+  res.render('sports', { sports })
+})
+
+app.use(express.static('htmlFiles'))
 
 app.use((req, res) => {
   res.status(404).render('404')
